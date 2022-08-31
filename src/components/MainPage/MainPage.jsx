1c4c8b06529aba162c103f1b96b9f88b5e8701d2
vincecustomz <vince.vuong@live.com>
2022-08-31 02:47:28 -0400

    starting features

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
-import UserLogOut from '../UserLogOut/UserLogOut'
+import React, { Component } from "react";
+import UserLogOut from "../UserLogOut/UserLogOut";
+import NavBar from "../NavBar/NavBar";
+
+import { Main } from "./MainPageElements";
 
 export default class MainPage extends Component {
-   
   render() {
     return (
-      <div>MainPage
-      <UserLogOut setUserInState={this.props.setUserInState}/>
-      </div>
-
-    )
+      <>
+        <NavBar />
+        <Main>
+          MainPage
+          <UserLogOut setUserInState={this.props.setUserInState} />
+        </Main>
+      </>
+    );
   }
 }
-
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Nav } from './NavBarElements'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <Nav>NavBar</Nav>
+    )
+  }
+}
